5e0de92e457c73b2fa5b96f5e97d35ade8531d4c
David Sierra <dsierra@digitalrealty.com>
2024-01-05 00:12:51 -0500

    Base script; parses options and filenames

---
+++ 01-wc-tool/ccwc.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"slices"
+	"strings"
+)
+
+var p = fmt.Println
+var legalOptions = []string{"c", "l", "w", "m"}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func removeDuplicates(slice []string) []string {
+	seen := make(map[string]bool)
+	result := []string{}
+
+	for _, char := range slice {
+		if _, present := seen[char]; !present {
+			seen[char] = true
+			result = append(result, char)
+		}
+	}
+	return result
+}
+
+func normalizeOptions(options []string) []string {
+	normalized := []string{}
+
+	for _, option := range options {
+		stripped := strings.TrimPrefix(option, "-")
+		if len(stripped) > 1 {
+			for _, char := range stripped {
+				normalized = append(normalized, string(char))
+			}
+		} else {
+			normalized = append(normalized, stripped)
+		}
+
+	}
+	return removeDuplicates(normalized)
+}
+
+func validateOptions(options []string) []string {
+	for _, flag := range options {
+		if !slices.Contains(legalOptions, flag) {
+			p("ccwc: illegal option", flag)
+			p("usage: ccwc [-clmw] [file ..]")
+			os.Exit(1)
+		}
+	}
+	return options
+}
+
+func main() {
+	args := os.Args[1:]
+	commandFlags := []string{}
+	fileNames := []string{}
+
+	for _, arg := range args {
+		if strings.HasPrefix(arg, "-") {
+			commandFlags = append(commandFlags, arg)
+		}
+		if !strings.HasPrefix(arg, "-") {
+			fileNames = append(fileNames, arg)
+		}
+	}
+
+	options := normalizeOptions(commandFlags)
+	validateOptions(options)
+
+	for _, file := range fileNames {
+		dat, err := os.ReadFile(file)
+		check(err)
+		p(file, "bytes:", len(dat))
+	}
+}
